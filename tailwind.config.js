7f8d96e3f4d883d301bfec6ef29a6728c184d35f
Milad <miladrst.developer@gmail.com>
2023-09-21 14:26:31 +0330

    "[update] colors extended & new fonts added"
    :wq

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+        cyber: "#ec1447"
+      },
+      fontFamily: {
+        iransans : ['iransans'],
+        yekan: ['yekan']
+      }
+    },
+  },
+  plugins: [],
+}
+
