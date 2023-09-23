5d83e2ef9e05132a2601d17219a48bc68414b209
Milad <miladrst.developer@gmail.com>
2023-09-23 15:57:15 +0330

    [update] tailwind plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
@@ -12,6 +15,67 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+   plugins: [
+    plugin(function({addBase, addComponents, addUtilities, matchUtilities, theme }){
+      
+      addBase({
+        ":root": {
+          "--stripes-rgb": "0 0 0",
+          "--stripes-angle": "-45deg",
+          "--stripes-opacity": "1",
+          "--stripes-size": "20px"
+        },
+        "@keyframes slide": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(var(--stripes-size))" }
+        }
+      })
+
+      // add components 
+
+      addComponents({
+         ".stripes": {
+            position: "relative",
+            overflow: "hidden",
+            isolation: "isolate"
+          },
+          ".stripes::before": {
+            "--stripes-color": "rgb(var(--stripes-rgb) / var(--stripes-opacity))",
+            position: "absolute",
+            zIndex: -1,
+            top: "0",
+            right: "0",
+            height: "100%",
+            width: "calc(100% + var(--stripes-size))",
+            content: '""',
+            backgroundImage: `linear-gradient( 
+              var(--stripes-angle), 
+              var(--stripes-color) 5%, 
+              transparent 5% 45%,
+              var(--stripes-color) 45% 55%, 
+              transparent 55% 95%, 
+              var(--stripes-color) 95% 100%)`,
+            backgroundSize: "var(--stripes-size) var(--stripes-size)",
+            animation: "slide 1s infinite linear"
+          }
+      })
+
+      // add utilities
+      addUtilities({
+        ".stripes-white": { "--stripes-rgb": "255 255 255" },
+        ".stripes-reverse": { "--stripes-angle": "45deg" }
+      })
+
+      //opacity modifiers
+      matchUtilities({
+        'stripes-opacity' : value => ({
+            '--stripes-opacity': value
+        })
+      } , {
+        values : theme('opacity')
+      })
+
+    })
+  ],
 }
 
